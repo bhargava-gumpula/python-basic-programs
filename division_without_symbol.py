3b87edb3bb525ee79981293f76e0b5ac37978962
Bhargava Gumpula <bhargavagumpula@Bhargavas-MacBook-Air.local>
2020-08-16 11:48:29 -0700

    Making sure that we do not divide by 0

--- division_without_symbol.py
+++ division_without_symbol.py
@@ -1,5 +1,8 @@
-
 def divide(dividend, divisor):
+
+   if divisor == 0:
+     return (dividend, -1)
+
    groups = 0
    while True:
       if divisor > dividend:
@@ -12,4 +15,27 @@ def divide(dividend, divisor):
 dividend = input("Enter Dividend:")
 divisor = input("Enter Divisor:")
 remainder, groups = divide(dividend, divisor)
-print ("%d / %d = %d | remainder = %d") % (dividend, divisor, groups, remainder)
+
+if groups == -1:
+  print ("%d / %d = undefined") % (dividend, divisor)
+else:
+  print ("%d / %d = %d | remainder = %d") % (dividend, divisor, groups, remainder)
+#===============================================================================================
+def divide(dividend, divisor):
+   if divisor == 0:
+      return dividend, -1
+   groups = 0
+   while divisor <= dividend:
+      dividend -= divisor
+      groups += 1
+   reaminder = dividend  
+   return reaminder, groups
+
+dividend = input("Enter Dividend:")
+divisor = input("Enter Divisor:")
+remainder, groups = divide(dividend, divisor)
+if groups == -1:
+  print ("%d / %d = undefined") % (dividend, divisor)
+else:
+  print ("%d / %d = %d | remainder = %d") % (dividend, divisor, groups, remainder)
+
