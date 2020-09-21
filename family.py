4c658b203086e918bbbc6b4f7212daf3e602b3fb
Bhargava Gumpula <bhargava.gumpula@gmail.com>
2020-09-20 20:45:04 -0700

    Adding exception to make more defensive

--- family.py
+++ family.py
@@ -1,9 +1,12 @@
 family_age = {
-              'Bhargava' : 9,
-              'Natasha' : 11,
-              'Mamatha' : 32,
-              'Suresh' : 39
+              'BHARGAVA' : 9,
+              'NATASHA' : 11,
+              'MAMATHA' : 32,
+              'SURESH' : 39
              }
-name = raw_input("What is your name?:")
-print ("Your age is %d") % (family_age[name])
 
+name = raw_input("What is your name?:")
+try:
+   print ("Your age is %d") % (family_age[name.upper()])
+except KeyError:
+   print "Key Not in Map. Try Again Later. Bye"
