9bd6ba70ff44b5670c94df0d02793d3af836ed60
Bhargava Gumpula <bhargava.gumpula@gmail.com>
2020-09-26 20:29:00 -0700

    Modified to use funtions.

--- turtle_images.py
+++ turtle_images.py
@@ -1,32 +1,40 @@
 import turtle
 import time
-
 t = turtle.Pen()
-t.forward(90)
-t.right(90)
-t.forward(90)
-t.right(90)
-t.forward(90)
-t.right(90)
-t.forward(90)
-t.right(90)
-t.forward(30)
-t.right(90)
-t.forward(90)
-t.left(90)
-t.forward(30)
-t.left(90)
-t.forward(90)
-t.right(90)
-t.forward(30)
-t.right(90)
-t.forward(30)
-t.right(90)
-t.forward(90)
-t.left(90)
-t.forward(30)
-t.left(90)
-t.forward(90)
+
+# Making outer box for grid
+def draw_outer_box():
+	t.forward(90)
+	t.right(90)
+	t.forward(90)
+	t.right(90)
+	t.forward(90)
+	t.right(90)
+	t.forward(90)
+	t.right(90)
+
+# Making horizontal lines for grid
+def draw_horiz_lines():
+	t.forward(30)
+	t.right(90)
+	t.forward(90)
+	t.left(90)
+	t.forward(30)
+	t.left(90)
+	t.forward(90)
+
+# Making vertical lines for grid
+def draw_verti_lines():
+	t.right(90)
+	t.forward(30)
+	t.right(90)
+	t.forward(30)
+	t.right(90)
+	t.forward(90)
+	t.left(90)
+	t.forward(30)
+	t.left(90)
+	t.forward(90)
 
 # Making function for turning around
 def turn_around():
@@ -34,25 +42,37 @@ def turn_around():
     t.left(90)
     t.left(90)
 
-#
-turn_around()   
-time.sleep(1)
-t.left(90)
-t.up()
-t.forward(90)
-t.down()
-t.forward(30)
-turn_around()
-t.forward(60)
-turn_around()
-t.forward(30)
-t.right(90)
-t.forward(30)
-turn_around()
-t.forward(60)
-turn_around()
-t.up()
-t.forward(120)
-t.down()
+# Making Plus sign
+def draw_plus_sign():
+	time.sleep(1)
+	t.left(90)
+	t.up()
+	t.forward(90)
+	t.down()
+	t.forward(30)
+	turn_around()
+	t.forward(60)
+	turn_around()
+	t.forward(30)
+	t.right(90)
+	t.forward(30)
+	turn_around()
+	t.forward(60)
+	turn_around()
+	t.up()
+	t.forward(120)
+	t.down()
+
+def main():
+   draw_outer_box()
+   draw_horiz_lines()
+   draw_verti_lines()
+   turn_around()
+   draw_plus_sign()
+
+if __name__ == "__main__":
+    main()
+
+# Asking user to exit
 raw_input("Enter anything to exit:")
 
