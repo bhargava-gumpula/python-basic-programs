631af41d831877c1c8ab1e9a3b35772bf89c5ada
Bhargava Gumpula <bhargava.gumpula@gmail.com>
2020-10-18 16:30:30 -0700

    Added a comment in mail send program

--- mail_sending.py
+++ mail_sending.py
@@ -1,12 +1,17 @@
 import smtplib
 
+# Before running this progam, you must go to the gmail manage account
+# and in security section, enable less secure apps to log in to gmail account
+
 GMAIL_USER = "bhargava.gumpula@gmail.com"
 GMAIL_PASS = "Rubicks.23_GC"
 SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465
+SMTP_PORT = 587
 
 def send_email(recipient, subject, text):
-    smtpserver = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+    smtpserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    smtpserver.ehlo()
+    smtpserver.starttls()
     smtpserver.ehlo()
     smtpserver.login(GMAIL_USER, GMAIL_PASS)
 
@@ -16,5 +21,5 @@ def send_email(recipient, subject, text):
     smtpserver.sendmail(GMAIL_USER, recipient, msg)
     smtpserver.close()
 
-send_email('bhargava_gumpula@gmail.com', 'Hello', '''Hi bhagha bee, 
+send_email('gsuryacse7k@gmail.com', 'Hello', '''Hi bhagha bee, 
                                                   are you hear?''')
