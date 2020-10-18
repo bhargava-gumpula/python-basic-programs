2ad306fdfb2f97e4ad05fe6ff7527906e05a2e6c
natasha gumpula <natasha@natashas-MacBook-Air.local>
2020-10-18 16:17:12 -0700

    Sending email using python

--- mail_sending.py
+++ mail_sending.py
@@ -1,16 +1,20 @@
 import smtplib
 
-GMAIL_USER = 'bhargava.gumpula@gmail.com'
-GMAIL_PASS = 'Rubicks.23_GC'
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
+GMAIL_USER = "bhargava.gumpula@gmail.com"
+GMAIL_PASS = "Rubicks.23_GC"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
 
 def send_email(recipient, subject, text):
-    smtpserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-    smtpserver.ehlo()
-    smtpserver.starttls()
+    smtpserver = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
     smtpserver.ehlo()
     smtpserver.login(GMAIL_USER, GMAIL_PASS)
 
-    header = 'To:' + recipent + '\n' + 'From: ' + GMAIL_USER
- 
+    header = 'To:' + recipient + '\n' + 'From: ' + GMAIL_USER
+    header += '\n' + 'Subject:' + subject + '\n'
+    msg = header + '\n' + text + ' \n\n'
+    smtpserver.sendmail(GMAIL_USER, recipient, msg)
+    smtpserver.close()
+
+send_email('bhargava_gumpula@gmail.com', 'Hello', '''Hi bhagha bee, 
+                                                  are you hear?''')
